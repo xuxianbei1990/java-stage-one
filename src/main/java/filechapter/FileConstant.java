c452c096e34b5d053ba97e2f749ce46a4dc9f624
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2021-08-30 17:30:52 +0800

    更新

--- src/main/java/filechapter/FileConstant.java
+++ src/main/java/filechapter/FileConstant.java
@@ -3,14 +3,15 @@ package filechapter;
 import java.io.IOException;
 
 public class FileConstant {
-	static final String TESTFILEPATH = "E:\\整理\\Java\\Lifecycle1.png";
-	static final String OUTFILEPATH = "E:\\整理\\Java\\delete.txt";
-	static final String READFILEPATH = "/filechapter/Read.txt";
-	//windows 默认txt文本是GBK
-	static final String READFILEPATHGBK = "E:\\整理\\Java\\ReadGBK.txt";
-	static final String READFILENCODEEPATH = "E:\\整理\\Java\\ReadEncode.txt";
-	
-	static void OpenFile(String FileValue) throws IOException {
-		Runtime.getRuntime().exec("rundll32 url.dll FileProtocolHandler file://" + FileValue);
-	}
+    static final String DIR_PATH = "E:\\整理\\Java";
+    static final String TESTFILEPATH = DIR_PATH + "\\Lifecycle1.png";
+    static final String OUTFILEPATH = DIR_PATH + "\\delete.txt";
+    static final String READFILEPATH = "/filechapter/Read.txt";
+    //windows 默认txt文本是GBK
+    static final String READFILEPATHGBK = DIR_PATH + "\\ReadGBK.txt";
+    static final String READFILENCODEEPATH = DIR_PATH + "\\ReadEncode.txt";
+
+    static void OpenFile(String FileValue) throws IOException {
+        Runtime.getRuntime().exec("rundll32 url.dll FileProtocolHandler file://" + FileValue);
+    }
 }
