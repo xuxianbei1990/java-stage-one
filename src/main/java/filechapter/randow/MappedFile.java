938aeb68531e5c447dbc698274e3b77e660441d7
xuxianbei1990 <34258324+xuxianbei1990@users.noreply.github.com>
2021-09-12 11:27:41 +0800

    新增ByteBuffer

--- src/main/java/filechapter/randow/MappedFile.java
+++ src/main/java/filechapter/randow/MappedFile.java
@@ -23,10 +23,12 @@ public class MappedFile {
     private int fileSize;
     private MappedByteBuffer mappedByteBuffer;
     private FileChannel fileChannel;
+    protected ByteBuffer writeBuffer = null;
+    protected TransientStorePool transientStorePool = null;
 
     protected final AtomicInteger wrotePosition = new AtomicInteger(0);
 
-    public MappedFile(final String fileName, final int fileSize) {
+    public MappedFile(final String fileName, final int fileSize, final TransientStorePool transientStorePool) {
         this.file = new File(fileName);
         ensureDirOk(file.getParent());
         try {
@@ -34,10 +36,58 @@ public class MappedFile {
             this.fileChannel = new RandomAccessFile(this.file, "rw").getChannel();
             //进行内存映射
             this.mappedByteBuffer = this.fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
+            this.writeBuffer = transientStorePool.borrowBuffer();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+
+    public int flush(final int flushLeastPages) {
+        //主要判断是否可以刷盘，如果有内容就刷盘，否则就不刷
+        if (this.isAbleToFlush(flushLeastPages)) {
+            //也不知道干啥，可能为了并发
+            if (hold()) {
+                try {
+                    if (this.fileChannel.position() != 0) {
+                        /**
+                         * 这里涉及到了操作系统知识：一般无论是fileChannel，mappedByteBuffer。
+                         * fileChannel.write(ByteBuffer.wrap(data));
+                         * byteBuffer.put(this.msgStoreItemMemory.array(), 0, maxBlank);
+                         * 之后，实际是给了操作系统，真正落盘的动作是操作系统完成的。
+                         * 所以可以自己手动force
+                         */
+                        this.fileChannel.force(false);
+                    } else {
+                        this.mappedByteBuffer.force();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                release();
+            }
+        }
+        return -1;
+    }
+
+    private void release() {
+
+
+    }
+
+    public int getReadPosition() {
+//        return this.writeBuffer == null ? this.wrotePosition.get() : this.committedPosition.get();
+        return 0;
+    }
+
+    private boolean hold() {
+        return false;
+    }
+
+
+    private boolean isAbleToFlush(int flushLeastPages) {
+        return false;
     }
 
     public void destroy() {
@@ -50,6 +100,12 @@ public class MappedFile {
         }
     }
 
+    /**
+     * 添加消息
+     *
+     * @param data
+     * @return
+     */
     public boolean appendMessage(final byte[] data) {
         try {
             fileChannel.position(this.wrotePosition.get());
@@ -58,6 +114,17 @@ public class MappedFile {
             e.printStackTrace();
         }
         this.wrotePosition.addAndGet(data.length);
+        return true;
+    }
+
+    public boolean appendMessagesInner(final byte[] data) {
+        try {
+            // 创建一个前面共享区域的buffer
+            ByteBuffer byteBuffer = writeBuffer != null ? writeBuffer.slice() : mappedByteBuffer.slice();
+            byteBuffer.put(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
--- src/main/java/filechapter/randow/TransientStorePool.java
+++ src/main/java/filechapter/randow/TransientStorePool.java
@@ -34,8 +34,20 @@ public class TransientStorePool {
             ByteBuffer byteBuffer = ByteBuffer.allocateDirect(fileSize);
             //把堆外的内存在操作系统中锁住 该批内存锁定，避免被置换到交换区，提高存储性能
             lockWinsResource((DirectBuffer) byteBuffer);
+            //offer 和 add 最大差别  offer 新增如果失败返回false。 而add如果失败会报错。因为容量的问题
             availableBuffers.offer(byteBuffer);
         }
+    }
+
+    public void returnBuffer(ByteBuffer byteBuffer) {
+        byteBuffer.position(0);
+        byteBuffer.limit(fileSize);
+        availableBuffers.offerFirst(byteBuffer);
+    }
+
+    public ByteBuffer borrowBuffer() {
+
+        return availableBuffers.pollFirst();
     }
 
     private void lockWinsResource(DirectBuffer byteBuffer) {
@@ -53,6 +65,27 @@ public class TransientStorePool {
             LibC.INSTANCE.munlock(pointer, new NativeLong(fileSize));
 
         }
+    }
+
+    public static void main(String[] args) {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(20);
+        byteBuffer.mark();
+        byteBuffer.put("qwertfghi".getBytes(), 0, "abcdefghi".length());
+        System.out.println(byteBuffer.position());
+        ByteBuffer byteBuffer2 = byteBuffer.slice();
+        byteBuffer2.put("ascdfg".getBytes(), 0, "ascdfg".length());
+        byte[] bytes = new byte["abcdefghi".length()];
+        byteBuffer.position(0);
+//        byteBuffer.reset();
+        byteBuffer.get(bytes, 0, "abcdefghi".length());
+        System.out.println(new String(bytes));
+        byte[] bytes2 = new byte[4];
+        byteBuffer2.rewind();
+        byteBuffer2.get(bytes2, 0, 4);
+        System.out.println(new String(bytes2));
+
+        byteBuffer.get(bytes2, 0, 4);
+        System.out.println(new String(bytes2));
     }
 
 }
